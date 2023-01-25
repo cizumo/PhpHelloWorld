5db699e9d0a36c288ebfedb549b261a6f80632d7
Colin Izumo <colin@datainsightpartners.com>
2023-01-24 17:35:00 -0800

    Initial commits

---
+++ demo/views/contact.view.php
@@ -0,0 +1,11 @@
+<?php include('partials/head.php') ?>
+<?php include('partials/nav.php') ?>
+<?php include('partials/banner.php') ?>
+
+<main>
+  <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+    <p>Now you are on the Contact page.</p>
+  </div>
+</main>
+
+<?php include('partials/footer.php') ?>
